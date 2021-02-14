67a62b8cfa9acf94cb67d66faa5293b341c240a9
Jasmine <jasmineguimaraes@gmail.com>
2021-02-14 16:48:42 -0300

    first upload

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/src/Navbar.jsx';
+
+function App() {
+  return (
+    <Navbar/>
+    
+  );
+}
+
+export default App;
